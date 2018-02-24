77fa7aee58cf1aa6abbfaeb702d5c027ca372a0d
Katie Vetzal <katrina.lara@vetzal.com>
2018-02-24 10:12:45 -0500

    Player Movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-	// Use this for initialization
+	private Rigidbody2D playerRB;
+
 	void Start () {
-		
+		playerRB = GetComponent<Rigidbody2D>();
 	}
 	
-	// Update is called once per frame
 	void Update () {
-		
+		if(Input.GetKeyDown(KeyCode.RightArrow)) {
+			playerRB.MovePosition(playerRB.position + Vector2.right);
+		}
+		if(Input.GetKeyDown(KeyCode.LeftArrow)) {
+			playerRB.MovePosition(playerRB.position + Vector2.left);
+		}
+		if(Input.GetKeyDown(KeyCode.UpArrow)) {
+			playerRB.MovePosition(playerRB.position + Vector2.up);
+		}
+		if(Input.GetKeyDown(KeyCode.DownArrow)) {
+			playerRB.MovePosition(playerRB.position + Vector2.down);
+		}
 	}
 }
