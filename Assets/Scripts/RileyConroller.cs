d8bbb3d215c868cd5ee17dfea3a2428d0dff0f86
Katie Vetzal <katrina.lara@vetzal.com>
2018-02-24 23:54:37 -0500

    Riley linked to Sprite

---
+++ Assets/Scripts/RileyConroller.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RileyConroller : MonoBehaviour {
+
+	private Riley riley;
+
+	void Start () {
+		riley = new Riley();
+	}
+	
+	void Update () {
+		
+	}
+}
